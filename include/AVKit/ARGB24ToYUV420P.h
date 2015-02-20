6f5f66228b04ea30cf6f97c0f8b1f78bbeb8f0c2
Tony Di Croce <dicroce@gmail.com>
2015-02-19 18:57:19 -0800

    Updated file headers.

--- include/AVKit/ARGB24ToYUV420P.h
+++ include/AVKit/ARGB24ToYUV420P.h
@@ -1,4 +1,14 @@
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// XSDK
+// Copyright (c) 2015 Schneider Electric
+//
+// Use, modification, and distribution is subject to the Boost Software License,
+// Version 1.0 (See accompanying file LICENSE).
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #ifndef __AVKit_ARGB24ToYUV420P_h
 #define __AVKit_ARGB24ToYUV420P_h
 
--- source/H264Decoder.cpp
+++ source/H264Decoder.cpp
@@ -1,4 +1,14 @@
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// XSDK
+// Copyright (c) 2015 Schneider Electric
+//
+// Use, modification, and distribution is subject to the Boost Software License,
+// Version 1.0 (See accompanying file LICENSE).
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #include "AVKit/H264Decoder.h"
 
 #include "XSDK/XException.h"
